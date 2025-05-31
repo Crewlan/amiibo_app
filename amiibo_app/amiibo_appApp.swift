c94c977f3a0591e519273bf5ea06184fc3187042
Crewlan <rodrigoa.work@gmail.com>
2025-05-30 23:44:05 -0300

    Initial Commit

---
+++ amiibo_app/amiibo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  amiibo_appApp.swift
+//  amiibo_app
+//
+//  Created by Rodrigo Silva on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct amiibo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
